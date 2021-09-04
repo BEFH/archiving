cfe943506bb1b139de3fd0b92c7d2538cf856321
BEFH <fultonh1@gmail.com>
2021-09-03 23:51:57 -0400

    ver

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='archive_goate',
-    version='1.0b3',
+    version='1.0b4',
     description='Automatic backup script for goate lab.',
     long_description=('Using DSMC to back up files is somewhat compled and\n'
                       'it\'s difficult to track what files and directories\n'

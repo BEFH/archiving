6332553b7f7fe094bb0c7246dfa45b215893fa2c
Brian Fulton-Howard <brian.fulton-howard@mssm.edu>
2024-02-26 12:29:48 -0500

    bump version

--- archive_goate.py
+++ archive_goate.py
@@ -29,7 +29,7 @@ import pandas as pd
 
 logtime = datetime.datetime.now().strftime('%d-%b-%Y_%H.%M')
 
-__version__ = '4.0'
+__version__ = '4.1'
 
 def get_type(ext, size, settings):
     types = settings['types']
